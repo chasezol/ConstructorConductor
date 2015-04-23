859c7319fa5b33c31252b1fac08b85d26236608f
chasezol <chase.zollinger@hotmail.com>
2015-04-22 20:38:42 -0600

    Completed Problems
    Completed Problems

--- sayName.js
+++ sayName.js
@@ -1,13 +1,21 @@
-//Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
+//Create a Person constructor that accepts name and age as parameters and sets those properties 
+//accordingly in the Constructor.
 
-  //code here
+  var Person = function(name, age){
+  this.name = name;
+  this.age = age;
+}
 
 
 //Now create three instances of Person with data you make up
 
-  //code here
+  var chase = new Person('Chase', '29');
+  var brandon = new Person('Brandon', '33');
+  var eric = new Person('Eric', '35');
 
+//Now add a sayName method on your Person class that will alert the name of whatever 
+//Person instance called it.
 
-//Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
-
-  //code here
+  Person.prototype.sayName = function(){
+       console.log(this.name);
+  }
